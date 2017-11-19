a455aaab5ddb0d7e778ee5a52ad7f050789f912d
Phalanstere <burckhardt@ludicmedia.de>
2017-11-19 11:42:16 +0100

    tween bug removed

--- functions/functions.js
+++ functions/functions.js
@@ -209,8 +209,7 @@ function Functions( div ) {
                             fontSize: "30px",
                             fontSize: "18px",
                             color: "blue",
-                            textAlign: "left",
-                            text: "return"
+                            textAlign: "left"
                            });  
 
 
